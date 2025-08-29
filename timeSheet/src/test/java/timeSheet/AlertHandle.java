2802718ca47588422d474e52b8be8f19b82dc222
itsmeshya <shyajayaraman@gmail.com>
2025-08-29 13:57:29 -0400

    Timesheet

---
+++ timeSheet/src/test/java/timeSheet/AlertHandle.java
@@ -0,0 +1,39 @@
+package timeSheet;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AlertHandle {
+	public static WebDriver driver;
+		public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+			driver=new ChromeDriver();
+			driver.get("https://demoqa.com/alerts");
+			driver.manage().window().maximize();
+			//driver.findElement(By.id("alertButton")).click();
+			
+			//driver.findElement(By.id("confirmButton")).click();
+			
+			//driver.findElement(By.id("promtButton")).click();
+						
+			driver.findElement(By.id("timerAlertButton")).click();
+			
+			Alert alt=driver.switchTo().alert();
+			
+			WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+			wait.until(ExpectedConditions.alertIsPresent());
+			//alt.accept();
+			//Thread.sleep(3000);
+			//alt.sendKeys("Shya");
+						
+			//alt.dismiss();
+			
+	}
+
+}
---
+++ timeSheet/src/test/java/timeSheet/Testbrowsercommands.java
@@ -0,0 +1,18 @@
+package timeSheet;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Testbrowsercommands {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		System.out.println("Title "+driver.getTitle());
+		System.out.println("Current URL "+driver.getCurrentUrl());
+		System.out.println("PageSource "+driver.getPageSource());
+		Thread.sleep(3000);
+		driver.close();
+	}
+}
